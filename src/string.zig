e5db3d7543aa00daabe3bbb9c1cab1cbb5e85431
Alexei Samokvalov <alexei@sam701.com>
2022-11-30 19:52:47 +0100

    Add literal strings

--- src/string.zig
+++ src/string.zig
@@ -5,31 +5,43 @@ const testing = std.testing;
 
 // Returned string is allocated
 pub fn parse(ctx: *Context) !?[]const u8 {
-    if (ctx.current()) |cur| {
-        if (cur == '"') {
-            var sctx = StringContext{ .output = std.ArrayList(u8).init(ctx.alloc), .escaped = false };
-            while (ctx.next()) |c| {
-                if (!try charTester(&sctx, c)) {
-                    _ = ctx.next();
-                    return sctx.output.toOwnedSlice();
-                }
-            }
-            return error.UnexpectedEOF;
-        } else {
-            return null;
+    return parseSingleLine(ctx);
+}
+
+pub fn parseSingleLine(ctx: *Context) !?[]const u8 {
+    var single_quote = false;
+    parser.consumeString(ctx, "\"") catch {
+        parser.consumeString(ctx, "'") catch return null;
+        single_quote = true;
+    };
+    var sctx = StringContext{
+        .output = std.ArrayList(u8).init(ctx.alloc),
+        .single_quote = single_quote,
+        .single_line = true,
+    };
+    errdefer sctx.output.deinit();
+
+    while (ctx.current()) |c| {
+        _ = ctx.next();
+        if (!try charTester(&sctx, c)) {
+            return sctx.output.toOwnedSlice();
         }
-    } else {
-        return null;
     }
+    return error.UnexpectedEOF;
 }
 
 const StringContext = struct {
     output: std.ArrayList(u8),
-    escaped: bool,
+    escaped: bool = false,
+    single_quote: bool,
+    single_line: bool,
 };
 
 // Returns true if further chars can be consumed.
 fn charTester(ctx: *StringContext, c: u8) !bool {
+    if (ctx.single_line) {
+        if(c == '\r' or c == '\n') return error.InvalidCharacter;
+    }
     if (ctx.escaped) {
         var cn = switch (c) {
             'b' => 0x08,
@@ -42,17 +54,40 @@ fn charTester(ctx: *StringContext, c: u8) !bool {
         try ctx.output.append(cn);
         ctx.escaped = false;
     } else {
-        switch (c) {
-            '"' => return false,
-            '\\' => {
-                ctx.escaped = true;
-            },
-            else => {
-                try ctx.output.append(c);
-            },
+        if (ctx.single_quote) {
+            if (c == '\'') return false;
+            try ctx.output.append(c);
+        } else {
+            switch (c) {
+                '"' => return false,
+                '\\' => {
+                    ctx.escaped = true;
+                },
+                else => {
+                    try ctx.output.append(c);
+                },
+            }
         }
     }
     return true;
+}
+
+test "single quote" {
+    var ctx = parser.testInput(
+        \\'\ab'=
+    );
+    var str = try parse(&ctx);
+    try testing.expect(std.mem.eql(u8, str.?, "\\ab"));
+    try testing.expect(ctx.current().? == '=');
+    ctx.alloc.free(str.?);
+}
+
+test "new lines in single line strings" {
+    var ctx = parser.testInput("'abc\n'");
+    try testing.expectError(error.InvalidCharacter, parse(&ctx));
+
+    ctx = parser.testInput("\"abc\n\"");
+    try testing.expectError(error.InvalidCharacter, parse(&ctx));
 }
 
 test "simple" {
@@ -104,6 +139,5 @@ test "simple escape" {
 // TODO: All other escape sequences not listed above are reserved; if they are used, TOML should produce an error.
 // TODO: unicode escaping \u0000
 // TODO: multi-line
-// TODO: literal string 'abc'
 // TODO: line ending backslash
 // TODO: multi-line literal string '''aoeu'''
