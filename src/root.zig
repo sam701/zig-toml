b59298f0f3ec9bcc8a413ad06af2f64d44d5d24c
Alexei Samokvalov <alexei@sam701.com>
2025-06-22 11:20:48 +0200

    Add an example to zig docs

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,39 @@
+//! ## Example
+//!
+//! ```
+//! const std = @import("std");
+//! const toml = @import("toml");
+//!
+//! var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+//! const allocator = gpa.allocator();
+//!
+//! const Address = struct {
+//!     port: i64,
+//!     host: []const u8,
+//! };
+//!
+//! const Config = struct {
+//!     master: bool,
+//!     expires_at: toml.DateTime,
+//!     description: []const u8,
+//!
+//!     local: *Address,
+//!     peers: []const Address,
+//! };
+//!
+//! pub fn main() anyerror!void {
+//!     var parser = toml.Parser(Config).init(allocator);
+//!     defer parser.deinit();
+//!
+//!     var result = try parser.parseFile("./examples/example1.toml");
+//!     defer result.deinit();
+//!
+//!     const config = result.value;
+//!     std.debug.print("{s}\nlocal address: {s}:{}\n", .{ config.description, config.local.host, config.local.port });
+//!     std.debug.print("peer0: {s}:{}\n", .{ config.peers[0].host, config.peers[0].port });
+//! }
+//! ```
+
 const std = @import("std");
 
 const parser = @import("./parser.zig");
