4a6bbb9e74a2a4ef52010cf095f00e20daf9723f
Alexei Samokvalov <alexei@sam701.com>
2025-09-05 09:12:00 +0300

    next

--- src/parser2.zig
+++ src/parser2.zig
@@ -10,6 +10,7 @@ pub const Parsed = std.json.Parsed;
 pub const Error = Scanner.Error || error{
     UnexpectedToken,
     NotStruct,
+    InvalidValueType,
 };
 
 pub fn parse(comptime T: type, reader: *Reader, alloc: Allocator) Error!Parsed(T) {
@@ -20,10 +21,13 @@ pub fn parse(comptime T: type, reader: *Reader, alloc: Allocator) Error!Parsed(T
     };
 }
 
+const StructTree = std.StringHashMap(StructTree);
+
 const Parser = struct {
     arena: *ArenaAllocator,
     scanner: *Scanner,
     token_location: ?SourceLocation,
+    struct_init_map: StructTree,
 
     pub fn init(reader: *Reader, alloc: Allocator) !Parser {
         const arena = try alloc.create(ArenaAllocator);
@@ -31,7 +35,12 @@ const Parser = struct {
         return .{
             .arena = arena,
             .scanner = try Scanner.init(reader, alloc),
+            .struct_init_map = StructTree.init(alloc),
         };
+    }
+
+    pub fn deinit(self: *Parser) void {
+        self.scanner.deinit();
     }
 
     fn nextToken(self: *Parser, expect_value: bool) Error!Token {
@@ -62,6 +71,36 @@ const Parser = struct {
             .end_of_document => {},
             else => return error.UnexpectedToken,
         }
+    }
+
+    fn parseKeyField(self: *Parser, comptime T: type, dest: *T, field_name: []const u8, init_map: *StructTree) Error!void {
+        const ti = @typeInfo(T);
+        if (ti != .@"struct") return error.NotStruct;
+
+        inline for (ti.@"struct".fields) |field| {
+            if (std.mem.eql(u8, field.name, field_name)) {
+
+                // ===
+
+                // ===
+                var token = try self.nextToken(false);
+                switch (token.kind) {
+                    .dot => {
+                        token = try self.nextToken(false);
+                        switch (token.kind) {
+                            .bare_key, .string => {},
+                            else => return error.UnexpectedToken,
+                        }
+                    },
+                    .equal => return self.parseInnerTable(T),
+                    else => return error.UnexpectedToken,
+                }
+
+                // @field(result, field.name) = try self.parseAfterBareKey(field.type);
+                return;
+            }
+        }
+        // TODO handle unmatched field
     }
 
     fn parseAfterBareKey(self: *Parser, comptime T: type) Error!T {
